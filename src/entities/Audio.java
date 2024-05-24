3e56ba28da826c0b9583eab324e81662d31b742f
ABBA3124 <davideabbadessa73@gmail.com>
2024-05-24 11:59:41 +0200

    classi audio e video

--- src/entities/Audio.java
+++ src/entities/Audio.java
@@ -1,4 +1,30 @@
 package entities;
 
-public class Audio {
+import interfaces.Riproducibile;
+
+public class Audio extends ElementoMultimediale implements Riproducibile {
+    private int durata;
+    private int volume;
+
+    public Audio(String titolo, int durata, int volume) {
+        super(titolo);
+        this.durata = durata;
+        this.volume = volume;
+    }
+
+    public void abbassaVolume() {
+        volume--;
+    }
+
+    public void alzaVolume() {
+        volume++;
+    }
+
+    @Override
+    public void play() {
+        for (int i = 0; i < durata; i++) {
+            System.out.println(titolo + "!".repeat(volume));
+
+        }
+    }
 }
--- src/entities/Video.java
+++ src/entities/Video.java
@@ -1,4 +1,39 @@
 package entities;
 
-public class Video {
+import interfaces.Riproducibile;
+
+public class Video extends ElementoMultimediale implements Riproducibile {
+    private int durata;
+    private int volume;
+    private int luminosita;
+
+    public video(String titolo, int durata, int volume, int luminosita) {
+        super(titolo);
+        this.durata = durata;
+        this.volume = volume;
+        this.luminosita = luminosita;
+    }
+
+    public void abbassaVolume() {
+        volume--;
+    }
+
+    public void alzaVolume() {
+        volume++;
+    }
+
+    public void aumentaLuminosita() {
+        luminosita++;
+    }
+
+    public void diminuisciLuminosita() {
+        luminosita--;
+    }
+
+    @Override
+    public void play() {
+        for (int i = 0; i < durata; i++) {
+            System.out.println(titolo + "!".repeat(volume) + "*".repeat(luminosita));
+        }
+    }
 }
